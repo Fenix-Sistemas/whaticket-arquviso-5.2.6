1d72bad074f3e100861e83362a5f389caf9a7867
Fenix-Sistemas <contato@fenixpr.com.br>
2024-09-11 16:45:57 -0300

    whaticket 5.2.6

--- frontend/src/pages/Signup/index.js
+++ frontend/src/pages/Signup/index.js
@@ -36,7 +36,7 @@ const Copyright = () => {
 		<Typography variant="body2" color="textSecondary" align="center">
 			{"Copyright © "}
 			<Link color="inherit" href="#">
-				PLW
+				Fenix
 			</Link>{" "}
 		   {new Date().getFullYear()}
 			{"."}
